83e0805d5dad4231c14a547e76239fa7b66246aa
jawad kazem <126715476+jawadnull@users.noreply.github.com>
2024-06-04 04:39:21 -0800

    insert_data.sh

---
+++ insert_data.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+
+
+
+# Function to get team ID from the teams table
+get_team_id() {
+  local team_name=$1
+  echo $($PSQL "SELECT team_id FROM teams WHERE name='$team_name'")
+}
+
+# Read games.csv and insert data
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != "year" ]]
+  then
+    # Insert unique teams into the teams table
+    WINNER_ID=$(get_team_id "$WINNER")
+    if [[ -z $WINNER_ID ]]
+    then
+      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERT_WINNER_RESULT == "INSERT 0 1" ]]
+      then
+        echo "Inserted into teams, $WINNER"
+      fi
+      WINNER_ID=$(get_team_id "$WINNER")
+    fi
+
+    OPPONENT_ID=$(get_team_id "$OPPONENT")
+    if [[ -z $OPPONENT_ID ]]
+    then
+      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
+      then
+        echo "Inserted into teams, $OPPONENT"
+      fi
+      OPPONENT_ID=$(get_team_id "$OPPONENT")
+    fi
+
+    # Insert data into the games table
+    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+    if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]
+    then
+      echo "Inserted into games, $YEAR $ROUND $WINNER vs $OPPONENT"
+    fi
+  fi
+done
